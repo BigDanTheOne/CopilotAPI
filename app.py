f3ed01f4c2b87f2b57f466feab5840288f449133
bigdan <230620ada>
2024-02-06 12:25:30 +0300

    asa

--- app.py
+++ app.py
@@ -1,133 +1,324 @@
+import socket
 import uuid
-from flask import Flask, request
-from flask_socketio import SocketIO, emit
-from flask_cors import CORS
-from output.test import *
-import queue
-from threading import Thread
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from pydub import AudioSegment
-import os
-import io
-from time import sleep
-from openai import OpenAI
+import asyncio
+from copilot import Copilot
+import socketio
+from queue import Queue as Q
+from fastapi.middleware.cors import CORSMiddleware
+import redis
+import paramiko
+import time
+import threading
+import select
 
-
-mic_audio_queue = queue.Queue()
-tab_audio_queue = queue.Queue()
-mic_audio_chunks = []
-tab_audio_chunks = []
-
-
-def _convert_to_ogg(filename):
-    # Загрузка исходного файла
-    audio = AudioSegment.from_file(filename)
-    # Конвертация в OGG
-    ogg_filename = os.path.splitext(filename)[0] + ".ogg"
-    audio.export(ogg_filename, format="ogg")
-    return ogg_filename
-
-
-def put_into_queue(data, mic=True):
-    # print(f'Chunk received, mic={mic}')
-    chunk = data['audio']
-    if mic:
-        mic_audio_chunks.append(chunk)
-        mic_audio_queue.put(chunk)
-    else:
-        tab_audio_chunks.append(chunk)
-        tab_audio_queue.put(chunk)
-
-
-def signal_end_of_stream():
-    mic_audio_queue.put(b'')
-
-
-ogg_filename = _convert_to_ogg('/Users/bigdan/cloudapi/darkcoding-webm-ebml.webm')
-ogg_audio = AudioSegment.from_file(ogg_filename, format="ogg")
-raw_audio = ogg_audio.set_frame_rate(8000).set_channels(1).set_sample_width(2).raw_data
-raw_audio_io = io.BytesIO(raw_audio)
+class SSHCredentias:
+    ssh_host = 'lorien.atp-fivt.org'
+    ssh_username = 'bohonkomi'
+    ssh_password = 'ncJUftkzearUC9YxNKK1'
+    local_port = 6011
+    remote_port = 6011
+    remote_host = 'localhost'
+    ssh_pkey = None
 
 
 frame_rate = 48000  # or whatever your frame rate is
-sample_width = 2    # sample width in bytes
-channels = 1        # mono=1, stereo=2
+sample_width = 2  # sample width in bytes
+channels = 1  # mono=1, stereo=2
+
+final_sample_rate = 16000
+final_sample_width = 2
+final_channels = 1
+
+app = FastAPI(debug=True, )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+class SessionStore(dict):
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+        self.r = redis.Redis(host='localhost', port=6379, db=0)
+        for key in self.r.keys():
+            print(self.r.keys())
+            s_id = self.r.get(key)
+            self[key] = {'session_id': s_id, 'queue': Q()}
 
 
-def audio_chunk_generator(mic=True):
-    yield stt_pb2.StreamingRequest(session_options=recognize_options)
-    # print("Ready")
-    while True:
-        if mic:
-            chunk = mic_audio_queue.get()
+    def __setitem__(self, key, value):
+        super().__setitem__(key, value)
+        if 'session_id' in value:
+            self.r.set(key, value['session_id'])
+
+    def __getitem__(self, key):
+        return super().__getitem__(key)
+
+    def __delitem__(self, key):
+        super().__delitem__(key)
+        self.r.delete(key)
+
+session_store = SessionStore()
+
+mgr = socketio.AsyncRedisManager()
+sio = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins='*', client_manager=mgr, logger=True)
+socket_app = socketio.ASGIApp(sio, other_asgi_app=app)
+app.mount("/", socket_app)
+
+
+def transform_audio(raw_audio):
+    audio_data = AudioSegment(
+        data=raw_audio,
+        sample_width=sample_width,
+        frame_rate=frame_rate,
+        channels=channels
+    )
+    audio_data = audio_data.set_frame_rate(final_sample_rate).set_channels(final_channels).set_sample_width(
+        final_sample_width)
+    audio_data = audio_data.raw_data
+    return audio_data
+
+
+@sio.event
+async def test_event(sid, message):
+    print("Test message received:", message)
+    await sio.emit('test_response', {'data': 'Hello from server!'}, to=sid)
+
+
+@sio.event
+async def connect(sid, environ):
+    print("Client connected", sid)
+    session_id = ''
+    # Extract the session_id from the query parameters
+    query_params = environ.get('QUERY_STRING', '')
+    for param in query_params.split('&'):
+        key, value = param.split('=')
+        if key == 'session_id':
+            session_id = value
+    # print("Client's session_is: ", session_id)
+    # print("Client's query: ", query_params)
+
+    if session_id and session_id in session_store:
+        while not session_store[session_id]['queue'].empty():
+            msg = session_store[session_id]['queue'].get()
+            await sio.emit(msg['msg_type'], msg['msg_content'], to=sid)
+        print(f"Reconnected client {sid} with existing session: {session_id}")
+    else:
+        # New session, generate a unique session_id
+        session_id = str(uuid.uuid4())  # or generate a new ID
+        session_store[session_id] = {'sid': sid, 'queue': Q()}  # initialize session data
+        print(f"New client {sid} connected with new session: {session_id}")
+
+    # Store the session_id in the socket's session
+    await sio.save_session(sid, {'session_id': session_id})
+    await sio.emit('session_id', session_id, to=sid)
+
+
+@sio.event
+async def disconnect(sid):
+    session = await sio.get_session(sid)
+    session_id = session.get('session_id')
+    del_worker(session_id)
+    del session_store[session_id]
+    print(f"Client {sid} disconnected. Session was: {session_id}")
+
+
+@sio.event
+async def new_session(sid, session_id_):
+    session = await sio.get_session(sid)
+    session_id = session.get('session_id')
+    del session_store[session_id]
+    if session_id != session_id_:
+        print(f"Alarm! Gotn {session_id_}, have {session_id}")
+    new_session_id = str(uuid.uuid4())
+    while new_session_id in session_store:
+        new_session_id = str(uuid.uuid4())
+    await sio.save_session(sid, {'session_id': session_id})
+    session_store[session_id] = {'sid': sid}  # initialize session data
+    await sio.emit('session_id', {'session_id': new_session_id}, to=sid)
+    print(f"New session with session_id {new_session_id} for sid {sid} was created")
+
+
+async def send_msg_async(msg_type, msg_content, session_id):
+    if session_id in session_store:
+        sid = session_store[session_id].get('sid')
+        if sid:
+            await sio.emit(msg_type, msg_content, to=sid)
         else:
-            chunk = tab_audio_queue.get()
-        if chunk == b'':
-            break
-        audio_data = AudioSegment(
-            data=chunk,
-            sample_width=sample_width,
-            frame_rate=frame_rate,
-            channels=channels
-        )
-        audio_data = audio_data.set_frame_rate(8000).set_channels(1).set_sample_width(2)
-        buffer = io.BytesIO()
-        audio_data.export(buffer, format="wav")  # Export as WAV format
-        buffer.seek(0)  # Reset
-        yield stt_pb2.StreamingRequest(chunk=stt_pb2.AudioChunk(data=buffer.read()))
-        # yield buffer.read()
+            session_store[session_id]['queue'].put({'msg_type': msg_type, 'msg_content': msg_content})
+    else:
+        pass  # TODO: what shell we do here?
+    print("end of async sending")
 
 
-app = Flask(__name__)
-CORS(app)
-socketio = SocketIO(app, cors_allowed_origins="*")
-cid_uid_map = {}
+def send_msg(msg_type, msg_content, session_id, loop=None):
+    print("inside send_msg")
+    if not loop:
+        loop = asyncio.get_event_loop()
+    return asyncio.run_coroutine_threadsafe(
+        send_msg_async(msg_type=msg_type,
+                       msg_content=msg_content,
+                       session_id=session_id),
+        loop
+    )
 
 
-mic_thread = False
-tab_thread = False
-YANDEX_KEY = "t1.9euelZqenZyVlcnLyY-KksuKjcyOje3rnpWals3Hyp3NlJKek5mMnJbNy5Tl8_cxch9U-e9oHnpi_t3z93EgHVT572geemL-zef1656Vmp3LnsyazMbOkMrJzciWz46R7_zF656Vmp3LnsyazMbOkMrJzciWz46R.QAuvu_rDaFhVFra4z5aJPcbT41huYMaux-RQ5fusU3DVlTAGxvF8YBSMbtrtSXdrP-G434JtknxZt29yL4R9DQ"
-mic_copilot = Copilot(YANDEX_KEY)
-tab_copilot = Copilot(YANDEX_KEY)
-worker_mic = Thread(target=mic_copilot.run, args=(audio_chunk_generator, True, socketio, cid_uid_map))
-worker_tab = Thread(target=tab_copilot.run, args=(audio_chunk_generator, False, socketio, cid_uid_map))
-worker_tab.start()
-worker_mic.start()
+def del_worker(session_id):
+    # if session_id in session_store and session_store[session_id].get('worker'):
+    #     session_store[session_id]['worker'].terminate()
+    #     session_store[session_id]['worker'].join()
+    #     del session_store[session_id]['worker']
+    pass # TODO
 
 
-@socketio.on('audio_data_mic')
-def handle_audio_data_mic(data):
-    mic_copilot.set_userid(data['user_id'])
-    global mic_thread
-    put_into_queue(data, mic=True)
-    if not mic_thread:
-        mic_thread = True
-
-@socketio.on('audio_data_tab')
-def handle_audio_data_tab(data):
-    tab_copilot.set_userid(data['user_id'])
-    global tab_thread
-    put_into_queue(data, mic=False)
-    if not tab_thread:
-        tab_thread = True
-
-@socketio.on('join')
-def handle_audio_data():
-    user_id = str(uuid.uuid4())
-    print(user_id)
-    cid_uid_map[user_id] = request.sid
-    print(cid_uid_map)
-    socketio.emit('connected', str(user_id), room=cid_uid_map[user_id])
+def new_worker(worker, session_id):
+    del_worker(session_id)
+    session_store[session_id]['worker'] = worker
+    session_store[session_id]['worker'].start()
 
 
-@app.route('/save-audio', methods=['POST'])
-def save_audio():
-    global audio_chunks
-    global isRecoding
-    isRecoding = False
-    if not audio_chunks:
-        return 'No audio data to save', 400
-    return 'Audio saved successfully', 200
+a1, a2 = 0, 0
+
+@sio.event
+async def audio_data_tab(sid, data):
+    global a2
+    session = await sio.get_session(sid)
+    session_id = session.get('session_id')
+    audio_data = transform_audio(data["audio"])
+    a2 += len(data["audio"])
+    print("tab: ", a2)
+    copilot.send(data={"data": audio_data, "session_id": session_id, "is_mic": False})
+
+@sio.event
+async def audio_data_mic(sid, data):
+    global a1
+    session = await sio.get_session(sid)
+    session_id = session.get('session_id')
+    audio_data = transform_audio(data["audio"])
+    a1 += len(data["audio"])
+    print("mic: ", a1)
+    copilot.send(data={"data": audio_data, "session_id": session_id, "is_mic": True})
 
 
-if __name__ == '__main__':
-    socketio.run(app, allow_unsafe_werkzeug=True)
+# def create_ssh_tunnel(username, password, host, remote_port, local_port):
+#     try:
+#         ssh = paramiko.SSHClient()
+#         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#         ssh.connect(host, username=username, password=password)
+#         print(f"SSH connection established to {host}")
+#
+#         # Здесь происходит фактическое создание проброса порта
+#         # ssh.get_transport().request_port_forward(host, remote_port)
+#         ssh.get_transport().open_channel("forwarded-tcpip", dest_addr=(host,
+#                                            remote_port), src_addr=('localhost', local_port))
+#
+#         print(f"Tunnel opened from localhost:{local_port} to localhost:{remote_port} through {host}")
+#
+#         # Бесконечный цикл для поддержания туннеля открытым
+#         while True:
+#             time.sleep(60)
+#     except Exception as e:
+#         print(f"Failed to create SSH tunnel: {e}")
+#     finally:
+#         ssh.close()
+
+# def forward_port(local_port, remote_host, remote_port, ssh_host, ssh_username, ssh_password=None, ssh_pkey=None):
+#     client = paramiko.SSHClient()
+#     client.load_system_host_keys()
+#     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#
+#     # Connect to the SSH server
+#     print(f"Connecting to SSH server {ssh_host} as {ssh_username}...")
+#     client.connect(hostname=ssh_host, username=ssh_username, password=ssh_password, pkey=ssh_pkey)
+#     print("Connected.")
+#
+#     # Set up port forwarding
+#     transport = client.get_transport()
+#     channel = transport.open_channel("direct-tcpip", (remote_host, remote_port), ("localhost", local_port))
+#
+#     try:
+#         while True:
+#             r, w, x = select.select([channel], [], [])
+#             if channel in r:
+#                 try:
+#                     data = channel.recv(1024)
+#                     if len(data) == 0:
+#                         print("\nConnection closed by remote host.")
+#                         break
+#                     else:
+#                         print(data)
+#                 except Exception as e:
+#                     print(f"\nError: {e}")
+#                     break
+#     finally:
+#         channel.close()
+#         client.close()
+#         print("SSH connection and port forwarding closed.")
+#
+#
+# def handler(chan, host, port):
+#     sock = socket.socket()
+#     try:
+#         sock.connect((host, port))
+#     except Exception as e:
+#         print(f"Forwarding request to {host}:{port} failed: {e}")
+#         return
+#     print(f"Connected! Tunnel open {host}:{port} -> {chan.origin_addr}")
+#
+#     while True:
+#         try:
+#             data = sock.recv(1024)
+#             if len(data) == 0:
+#                 break
+#             chan.send(data)
+#         except Exception as e:
+#             print(f"Connection to {host}:{port} closed: {e}")
+#             break
+#     chan.close()
+#     sock.close()
+#
+# def forward_tunnel(local_port, remote_host, remote_port, transport):
+#     transport.request_port_forward("", local_port)
+#     while True:
+#         chan = transport.accept(1000)
+#         if chan is None:
+#             continue
+#         thr = threading.Thread(target=handler, args=(chan, remote_host, remote_port))
+#         thr.setDaemon(True)
+#         thr.start()
+#
+# def ssh_port_forward(local_port, remote_host, remote_port, ssh_host, ssh_username, ssh_password, ssh_key=None):
+#     client = paramiko.SSHClient()
+#     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#     client.load_system_host_keys()
+#
+#     print(f"Connecting to SSH server {ssh_host}...")
+#     client.connect(ssh_host, username=ssh_username, password=ssh_password, key_filename=ssh_key)
+#     print("SSH connection established.")
+#
+#     transport = client.get_transport()
+#     forward_thread = threading.Thread(target=forward_tunnel, args=(local_port, remote_host, remote_port, transport))
+#     forward_thread.setDaemon(True)
+#     forward_thread.start()
+#
+#     print(f"Port forwarding setup: 'localhost:{local_port}' to '{remote_host}:{remote_port}' through SSH.")
+#     forward_thread.join()
+
+
+if __name__ == "__main__":
+    from uvicorn import Config, Server
+    # threading.Thread(target=ssh_port_forward, args=(SSHCredentias.local_port, SSHCredentias.remote_host, SSHCredentias.remote_port,
+    #                                             SSHCredentias.ssh_host, SSHCredentias.ssh_username, SSHCredentias.ssh_password,
+    #                                             SSHCredentias.ssh_pkey)).start()
+    # time.sleep(5)
+    main_loop = asyncio.new_event_loop()
+    copilot = Copilot(send_msg, new_worker, main_loop)
+    copilot.run()
+
+    config = Config(app=app, loop=main_loop)
+    server = Server(config)
+    main_loop.run_until_complete(server.serve())
+    # uvicorn.run(app, host="127.0.0.1", port=8000)
---
+++ copilot.py
@@ -0,0 +1,177 @@
+import threading
+import time
+import json
+import socket
+import pickle
+from openai import OpenAI
+
+
+msgs = [
+    "Михаил, спасибо за вашу честность. Я понимаю, что у вас возникли определенные предубеждения по отношению к нашей продукции. Это нормально, когда люди скептически относятся к чему-то новому или к продуктам, о которых слышали смешанные отзывы. Я был бы рад поделиться с вами актуальной информацией о наших пылесосах и тех инновациях, которые мы внедрили в модель Kirby 9000. Это поможет вам сформировать свое мнение на основе фактов. Может быть, есть конкретные моменты или вопросы, которые вызывают у вас сомнения? Это позволит мне дать более развернутый и точный ответ.",
+    "Михаил, я понимаю, что у вас уже есть пылесос, и это важно. Однако, как специалист в этой области, я хотел бы отметить, что наш новый Kirby 9000 предлагает некоторые уникальные функции и технологии, которые могут не только упростить уборку, но и существенно улучшить качество воздуха в вашем доме. Это особенно актуально в наше время, когда важность чистого воздуха и гигиены в доме повышается. Возможно, вы бы хотели узнать о некоторых ключевых особенностях нашего продукта, которые могут предложить вам нечто большее, чем ваш текущий пылесос? Например, способность уничтожать вирусы и аллергены может быть особенно полезной для вашего дома.",
+    "Михаил, это отлично, что у вас нет аллергии. Здоровье и комфорт в доме действительно важны. Наш пылесос Kirby 9000 не только помогает людям с аллергией, но и обеспечивает более глубокую и эффективную уборку благодаря передовым технологиям. Например, он оснащен системой фильтрации последнего поколения, которая позволяет улавливать даже мельчайшие частицы пыли и грязи, что обычные пылесосы не всегда могут сделать. Могу я спросить, какие задачи уборки у вас наиболее часто возникают дома? Возможно, наш продукт имеет конкретные функции, которые могут существенно упростить эти процессы для вас.",
+    "Михаил, благодарю за эту информацию. Понимаю, что уход за паркетом требует особого подхода, чтобы сохранить его красоту и продлить срок службы. Наш пылесос Kirby 9000 оснащен специальными насадками для ухода за паркетом, которые бережно удаляют пыль и грязь, не царапая поверхность. Кроме того, он обладает системой регулировки мощности всасывания, что позволяет эффективно очищать паркет, не повреждая его. Если вы разрешите, я могу показать вам, как Kirby 9000 работает на паркете прямо у вас дома, чтобы вы могли увидеть результат и оценить удобство использования лично. Как вам такая идея?",
+    "Ручные пылесосы, вроде Kirby 9000, удобнее и мобильнее обычных благодаря их компактности и отсутствию проводов. Они также мощнее и эффективнее в уборке, собирая больше пыли и грязи, и оснащены специальными насадками для разных поверхностей."
+        ]
+
+
+class ChatGPTWorker(threading.Thread):
+    def __init__(self, session_id, messages, socket, llm, mic, send_msg, loop):
+        super().__init__()
+        self.session_id = session_id
+        self.messages = messages
+        self.socket = socket
+        self.llm = llm
+        self.mic = mic
+        self.send_msg = send_msg
+        self._stop_event = threading.Event()
+        self.loop = loop
+    def stop(self):
+        self._stop_event.set()
+
+    def stopped(self):
+        return self._stop_event.is_set()
+
+    def run(self):
+        print(f"Starting worker for client {self.session_id}")
+        try:
+            if not self.stopped():
+                completion = self.llm.chat.completions.create(
+                    model="gpt-3.5-turbo-1106",
+                    messages=self.messages,
+                    stream=True
+                )
+                if self.mic == 'feedback':
+                    print('>>>>>>>>>>Feedback:')
+                    ans = ''
+                    for chunk in completion:
+                        if chunk.choices[0].delta.content:
+                            ans += chunk.choices[0].delta.content
+                            print('feedback: ', ans, self.messages)
+                            self.send_msg(msg_type='feedback', msg_content={'response': ans}, session_id=self.session_id, loop=self.loop)
+
+                    return
+                if self.mic:
+                    print('User hint form ChatGPT: ')
+                else:
+                    print('Interviewer hint from ChatGPT: ')
+                ans = ''
+                l = 0
+                for m in self.messages:
+                    if m["role"] == "user":
+                        l += 1
+                for chunk in completion:
+                    if chunk.choices[0].delta.content:
+                        ans += chunk.choices[0].delta.content
+                        print(ans, self.messages)
+                        self.send_msg(msg_type='copilot',
+                                      msg_content={'session_id': self.session_id, 'response': ans, 'len': l},
+                                      session_id=self.session_id,
+                                      loop=self.loop)
+        except Exception as e:
+            print(e)
+        finally:
+            print(f"Worker for client {self.session_id} stopped")
+
+class Copilot:
+    def __init__(self, send_msg, new_worker, loop, host='127.0.0.1', port=6005, OPENAI_API_KEY = "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"):
+        self.mic_history = []
+        self.tab_history = []
+        self.gpt_messages = []
+        self.history = []
+        self.lock = threading.Lock()
+        self.host = host
+        self.port = port
+        self.send_msg = send_msg
+        self.new_worker = new_worker
+        self.llm = OpenAI(api_key=OPENAI_API_KEY)
+        self.stop_event = None
+        self.s = None
+        self.loop = loop
+        self.uber_lock = threading.Lock()
+
+
+    def update(self, session_id, data, is_mic):
+        if data == "":
+            return
+        print(">>>>>>>>>>>>>>>>" + data, is_mic)
+        if is_mic:
+            self.mic_history.append(data)
+            self.history.append(['mic', data])
+            # print("Mic:>>>>>>>>>>>" + data)
+            self.send_msg(msg_type="mic_transcript",
+                          msg_content={'session_id': session_id, 'response': data, 'len': len(self.mic_history)},
+                          session_id=session_id,
+                          loop=self.loop)
+            print("send_msg finished")
+        else:
+            prompt_base = """
+Веди себя как опытный менеджер по продажам с 20-летним стажем в KIRBY - американской компании с представительствами по всему миру. Твоя задача - анализировать реплики клиента и предлагать конструктивные ответы, которые преодолевают возражения и продвинуть сделку. 
+При отсутствии возражений - предлагай стратегию дальнейшего развития диалога, направленную на установление контакта, выявление потребностей и мотивацию клиента. 
+Фокусируйся на построении доверия, вникании в ситуацию клиента, предложении взаимовыгодных решений. Избегай навязчивости и излишнего давления. 
+В ответе формулируй конкретное высказывание для менеджера применительно к текущему моменту диалога с клиентом. Учитывай контекст и эмоциональный окрас беседы. Цель одна - "заполучить клиента". Каждый твой ответ - лишь ответ как человека - без ненужной информации или вводных слов модели перед настоящим ответом.
+            """
+            self.tab_history.append(data)
+            self.history.append(['tab', data])
+            self.send_msg(msg_type="tab_transcript",
+                          msg_content = {'session_id': session_id, 'response': data, 'len': len(self.tab_history)},
+                          session_id=session_id,
+                          loop=self.loop)
+            messages = [
+                {"role": "system",
+                 "content": prompt_base},
+            ]
+            for i, msg in enumerate(self.history):
+                if msg[0] == 'tab':
+                    messages.append({"role": "user", "content": msg[1]})
+                else:
+                    messages.append({"role": "assistant", "content": msg[1]})
+            # self.new_worker(ChatGPTWorker(session_id, messages, socket, self.llm, is_mic, self.send_msg, self.loop),
+            #                 session_id)
+
+
+    def send(self, data=None):
+        with self.uber_lock:
+            data = pickle.dumps(data)
+            self.s.sendall(data)
+            time.sleep(0.03)
+
+    def receiver(self):
+        try:
+            while True: # постоянно, пока не стопнули
+                data = bytearray()  # Используем bytearray для накопления данных
+                while True:
+                    chunk = self.s.recv(1024)  # приняли чанк
+                    print("data recvd", data)
+                    data.extend(chunk)
+                    if (len(chunk) < 1024) and (
+                            len(chunk) > 0):  # если пакет пуст или неполный, то это последний или некорректный чанк
+                        break
+
+                if len(data) > 0:
+                    try:
+                        data = pickle.loads(data)  # расшифровываем данные
+                        session_id = data["session_id"]
+                        is_mic = data["is_mic"]
+                        data = data["data"]
+                    except:
+                        print("error to unpack")
+                    else:
+                        self.update(session_id, data, is_mic)
+                    # threading.Thread(target=self._process_input_data, args=(data,)).start()
+                else:
+                    print("nothing")
+        except OSError as e:
+            print("Error in receiving messages:", e)
+
+
+    def start_server(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.s:
+            self.s.connect((self.host, self.port))
+            print(f"Connected by {self.s}")
+            self.receiver()
+
+
+    def run(self):
+        server = threading.Thread(target=self.start_server)
+        server.start()
